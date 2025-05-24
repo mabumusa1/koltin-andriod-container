a0892497dbc5fe44da860c62e3e51d3109c31bdd
Mohammad Abu Musa <m.abumusa@gmail.com>
2025-05-24 19:28:49 +0000

    handle version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "karage.app"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "karage.app"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
