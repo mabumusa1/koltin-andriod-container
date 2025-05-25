d7bd31f418923e7ade9a8f57de6d75dbd960b019
Mohammad Abu Musa <m.abumusa@gmail.com>
2025-05-25 05:39:49 +0000

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,27 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.gradle.versions)
     id("io.gitlab.arturbosch.detekt") version "1.23.6" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.2" apply false
     id("org.jetbrains.dokka") version "2.0.0" apply false
+}
+
+// Configure dependency updates plugin
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    checkForGradleUpdate = true
+    outputFormatter = "json"
+    outputDir = "build/reports/dependencyUpdates"
+    reportfileName = "report"
+    
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }
